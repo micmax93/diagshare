931527dde39b2ab2c8c2a48846a76346c04406d9
Marcin Biernacki <mb@x.yum.pl>
2013-03-23 15:27:48 +0100

    Reorganizacja JS: window.

--- iwm/application/views/canvas.php
+++ iwm/application/views/canvas.php
@@ -1,31 +1,5 @@
 <?php
 
-
-foreach ($images as $k => $v) {
-    $size = getimagesize($v);
-    echo '
-    <div class="imageWindow" id="' . $k . '_window"  onmousedown="firstPlanByName(\'' . $k . '_window\');">
-        <div class="titleBar" onmousedown="firstPlanByName(\'' . $k . '_window\');"><p>' . $k . ' [' . $size[0] . 'x' . $size[1] . ']</p>
-        Zoom:
-        <a href="javascript:zoomGrid(\'' . $k . '_grid\',1.1);" class="smallButton">+</a>
-        <a href="javascript:zoomGrid(\'' . $k . '_grid\',0.9);" class="smallButton">-</a>
-        <!--B:
-        <a href="javascript:changeFilter(\'' . $k . '_img\',1,0,0);" class="smallButton">+</a>
-        <a href="javascript:changeFilter(\'' . $k . '_img\',-1,0,0);" class="smallButton">-</a>
-        C:
-        <a href="javascript:changeFilter(\'' . $k . '_img\',0,1,0);" class="smallButton">+</a>
-        <a href="javascript:changeFilter(\'' . $k . '_img\',0,-1,0);" class="smallButton">-</a>
-        O:
-        <a href="javascript:changeFilter(\'' . $k . '_img\',0,0,1);" class="smallButton">+</a>
-        <a href="javascript:changeFilter(\'' . $k . '_img\',0,0,-1);" class="smallButton">-</a>
-        Reset: <a href="javascript:resetFilter(\'' . $k . '_img\');" class="smallButton">R</a>-->
-        <a href="javascript:showHide(\'' . $k . '_window\');" class="close">X</a></div>
-        <div class="viewport" id="' . $k . '_viewport">
-            <div class="imageGrid" id="' . $k . '_grid">
-            </div>
-        </div>
-    </div>';
-}
 echo '
 <script type="text/javascript">
 var graphic = new Array();
@@ -34,33 +8,16 @@ var zdjecia = new Array();
 var tc;
 
 ';
-$i = 0;
 foreach ($images as $k => $v) {
     $size = getimagesize($v);
     $size[0] = $size[0] * $imageSettings["scale"];
     $size[1] = $size[1] * $imageSettings["scale"];
 
-    echo "
-        // Okno " . $k . "
-        $('#" . $k . "_window').draggable();
-        $('#" . $k . "_window').css('top'," . ($i * 20) . ");
-        $('#" . $k . "_window').width(" . $size[0] . ");
-        $('#" . $k . "_window').height(" . ($size[1] + 20) . " );
-        $('#" . $k . "').width(" . $size[0] . ");
-        $('#" . $k . "').height(" . $size[1] . ");
-
-        tc = addCanvas('" . $k . "_grid','" . $k . "_img');
-        tc.addEventListener('mousedown',function() {firstPlanByName('" . $k . "_window');},false);
-        drawOnCanvas('" . $k . "_img','" . $v . "',1);
-        $('#" . $k . "_grid').draggable();
-        $('#" . $k . "_viewport').css('z-index',0);
-        $('#" . $k . "').css('z-index',0);
-        listaOkien.push('" . $k . "_window');
-
-    ";
-    $i++;
-
+    echo '
+        createWindow(\'main\',\'' . $k . '\',' . $size[0] . ',' . $size[1] . ',\'' . $v . '\');
+    ';
 }
+
 echo '
 </script>
 
--- iwm/application/views/js/windows.js
+++ iwm/application/views/js/windows.js
@@ -1,10 +1,56 @@
 /**
- * Created with JetBrains PhpStorm.
  * User: phisikus
  * Date: 23.03.13
  * Time: 14:50
- * To change this template use File | Settings | File Templates.
  */
+var listaOkien = Array();
+
+
+function createWindow(parentId, id, width, height, imgUrl) {
+
+    $('#' + parentId).append('' +
+        '<div class="imageWindow" id="' + id + '_window"  onmousedown="firstPlanWindow(\'' + id + '_window\');">' +
+        '   <div class="titleBar" onmousedown="firstPlanWindow(\'' + id + '_window\');"><p>' + id + ' [' + width + 'x' + height + ']</p>' +
+        '       <a href="javascript:zoomGrid(\'' + id + '_grid\',1.1);" class="smallButton">+</a>' +
+        '       <a href="javascript:zoomGrid(\'' + id + '_grid\',0.9);" class="smallButton">-</a>' +
+        '       <a href="javascript:closeWindow(\'' + id + '_window\');" class="close">X</a>' +
+        '   </div>' +
+        '   <div class="viewport" id="' + id + '_viewport">' +
+        '       <div class="imageGrid" id="' + id + '_grid">' +
+        '       </div>' +
+        '   </div>' +
+        '</div>' +
+        '');
+    listaOkien.push(id + '_window');
+    $('#' + id + '_window').draggable().css('top', (listaOkien.length * 20)).width(width).height(height + 20);
+    $('#' + id).width(width).height(height);
+
+
+    tc = addCanvas(id + '_grid', id + '_img');
+    tc.addEventListener('mousedown', function () {
+        firstPlanWindow(id + '_window');
+    }, false);
+    drawOnCanvas(id + '_img', imgUrl, 1);
+    $('#' + id + '_grid').draggable();
+    $('#' + id + '_viewport').css('z-index', 0);
+    $('#' + id).css('z-index', 0);
+    listaOkien.push(id + '_window');
+}
+
+
+function closeWindow(id) {
+    $('#' + id).css('display', 'none');
+}
+
+
+function firstPlanWindow(name) {
+    for (var i in listaOkien) {
+        document.getElementById(listaOkien[i]).style.zIndex = 0;
+    }
+    document.getElementById(name).style.zIndex = 1;
+
+}
+
 
 function showHide(thing) {
     var el = document.getElementById(thing);
@@ -20,12 +66,3 @@ function showHide(thing) {
 
 }
 
-var listaOkien = Array();
-
-function firstPlanByName(name) {
-    for (var i in listaOkien) {
-        document.getElementById(listaOkien[i]).style.zIndex = 0;
-    }
-    document.getElementById(name).style.zIndex = 1;
-
-}
