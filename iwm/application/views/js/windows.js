dbc9491cba4bd8654f05374862031b819de5b43d
Marcin Biernacki <mb@x.yum.pl>
2013-03-23 22:52:29 +0100

    Poprawka zoom'u dla gridu zdjęć.

--- iwm/application/views/js/windows.js
+++ iwm/application/views/js/windows.js
@@ -18,7 +18,7 @@ function createWindow(parentId, id, width, height, rows, rowSize, images) {
     // Dodaj diva ze strukturą okna
     $('#' + parentId).append('' +
         '<div class="imageWindow" id="' + id + '_window"  onmousedown="firstPlanWindow(\'' + id + '_window\');">' +
-        '   <div class="titleBarActive" onmousedown="firstPlanWindow(\'' + id + '_window\');"><p>' + id + ' [' + width*rowSize + 'x' + height*rows + ']</p>' +
+        '   <div class="titleBarActive" onmousedown="firstPlanWindow(\'' + id + '_window\');"><p>' + id + ' [' + width * rowSize + 'x' + height * rows + ']</p>' +
         'Zoom:' +
         '       <a href="javascript:zoom(\'' + id + '_grid\',1.1);" class="smallButton">+</a>' +
         '       <a href="javascript:zoom(\'' + id + '_grid\',0.9);" class="smallButton">-</a>' +
@@ -33,7 +33,8 @@ function createWindow(parentId, id, width, height, rows, rowSize, images) {
 
     // Dodaj okno do listy i umożliw jego przesuwanie a także zwiększ o wysokość paska tytułowego
     listaOkien.push(id + '_window');
-    $('#' + id + '_window').draggable().css('top', (listaOkien.length * 20)).width(width*rowSize).height(height*rows + 20);
+    $('#' + id + '_window').draggable().css('top', (listaOkien.length * 20)).width(width * rowSize).height(height * rows + 20);
+    $('#' + id + '_grid').width(width * rowSize).height(height * rowSize);
 
     // Dodaj kanwę
 
@@ -42,18 +43,15 @@ function createWindow(parentId, id, width, height, rows, rowSize, images) {
         // Rysuj zdjęcie
         drawOnCanvas(id + '_img', images, 1);
 
-
     }
-    else
-    {
-        for(var i=0;i<images.length;i++)
-        {
+    else {
+        for (var i = 0; i < images.length; i++) {
 
-            tc = addCanvas(id + '_grid', id + '_img'+i);
-            if(((i+1)%rowSize) == 0)
-                addNewLine(id+'_grid');
+            tc = addCanvas(id + '_grid', id + '_img' + i);
+            if (((i + 1) % rowSize) == 0)
+                addNewLine(id + '_grid');
             // Rysuj zdjęcie
-            drawOnCanvas(id + '_img' +i, images[i], 1);
+            drawOnCanvas(id + '_img' + i, images[i], 1);
         }
 
     }
@@ -76,10 +74,9 @@ function createWindow(parentId, id, width, height, rows, rowSize, images) {
 
 function closeWindow(id) {
     $('#' + id).css('display', 'none');
-    for(var i=0;i<listaOkien.length; i++)
-    {
-        if(listaOkien[i] == id)
-            listaOkien.splice(i,1);
+    for (var i = 0; i < listaOkien.length; i++) {
+        if (listaOkien[i] == id)
+            listaOkien.splice(i, 1);
     }
 }
 
